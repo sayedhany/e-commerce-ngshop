274a3201714179f04274b335004b181882735cbf
sayedhany <shany@kn-it.com>
2024-03-19 13:19:26 +0200

    aha

--- libs/orders/src/lib/services/cart.service.ts
+++ libs/orders/src/lib/services/cart.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Injectable } from '@angular/core';
-
+import { Cart, CartItem } from '../models/cart';
+export const CART_KEY = 'cart';
 @Injectable({
     providedIn: 'root'
 })
@@ -10,6 +11,12 @@ export class CartService {
         const initCart = {
             items: []
         };
-        // localStorage.setItem('cart', JSON.stringify(initCart));
+        localStorage.setItem('cart', JSON.stringify(initCart));
+    }
+    setCartItem(cartItem: CartItem): Cart {
+        const cart: Cart = JSON.parse(localStorage.getItem(CART_KEY));
+        cart.items.push(cartItem);
+
+        return cart;
     }
 }
